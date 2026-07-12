f6b9eece6b40c7d76227291ca24fd01940aeb3c9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,39 @@
-output "machine_learning_datastore_blobstorages" {
-  description = "All machine_learning_datastore_blobstorage resources"
-  value       = azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages
-  sensitive   = true
-}
 output "machine_learning_datastore_blobstorages_account_key" {
-  description = "List of account_key values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.account_key]
+  description = "Map of account_key values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.account_key }
   sensitive   = true
 }
 output "machine_learning_datastore_blobstorages_description" {
-  description = "List of description values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.description]
+  description = "Map of description values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.description }
 }
 output "machine_learning_datastore_blobstorages_is_default" {
-  description = "List of is_default values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.is_default]
+  description = "Map of is_default values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.is_default }
 }
 output "machine_learning_datastore_blobstorages_name" {
-  description = "List of name values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.name]
+  description = "Map of name values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.name }
 }
 output "machine_learning_datastore_blobstorages_service_data_auth_identity" {
-  description = "List of service_data_auth_identity values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.service_data_auth_identity]
+  description = "Map of service_data_auth_identity values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.service_data_auth_identity }
 }
 output "machine_learning_datastore_blobstorages_shared_access_signature" {
-  description = "List of shared_access_signature values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.shared_access_signature]
+  description = "Map of shared_access_signature values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.shared_access_signature }
   sensitive   = true
 }
 output "machine_learning_datastore_blobstorages_storage_container_id" {
-  description = "List of storage_container_id values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.storage_container_id]
+  description = "Map of storage_container_id values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.storage_container_id }
 }
 output "machine_learning_datastore_blobstorages_tags" {
-  description = "List of tags values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.tags]
+  description = "Map of tags values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.tags }
 }
 output "machine_learning_datastore_blobstorages_workspace_id" {
-  description = "List of workspace_id values across all machine_learning_datastore_blobstorages"
-  value       = [for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : v.workspace_id]
+  description = "Map of workspace_id values across all machine_learning_datastore_blobstorages, keyed the same as var.machine_learning_datastore_blobstorages"
+  value       = { for k, v in azurerm_machine_learning_datastore_blobstorage.machine_learning_datastore_blobstorages : k => v.workspace_id }
 }
 
