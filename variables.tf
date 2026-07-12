04a1ca0a5d8a49ae204d86365715bf806b530bc9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:44 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -7,23 +7,31 @@ Required:
     - workspace_id
 Optional:
     - account_key
+    - account_key_key_vault_id (alternative to account_key - read from Key Vault instead)
+    - account_key_key_vault_secret_name (alternative to account_key - read from Key Vault instead)
     - description
     - is_default
     - service_data_auth_identity
     - shared_access_signature
+    - shared_access_signature_key_vault_id (alternative to shared_access_signature - read from Key Vault instead)
+    - shared_access_signature_key_vault_secret_name (alternative to shared_access_signature - read from Key Vault instead)
     - tags
 EOT
 
   type = map(object({
-    name                       = string
-    storage_container_id       = string
-    workspace_id               = string
-    account_key                = optional(string)
-    description                = optional(string)
-    is_default                 = optional(bool)   # Default: false
-    service_data_auth_identity = optional(string) # Default: "None"
-    shared_access_signature    = optional(string)
-    tags                       = optional(map(string))
+    name                                          = string
+    storage_container_id                          = string
+    workspace_id                                  = string
+    account_key                                   = optional(string)
+    account_key_key_vault_id                      = optional(string)
+    account_key_key_vault_secret_name             = optional(string)
+    description                                   = optional(string)
+    is_default                                    = optional(bool)   # Default: false
+    service_data_auth_identity                    = optional(string) # Default: "None"
+    shared_access_signature                       = optional(string)
+    shared_access_signature_key_vault_id          = optional(string)
+    shared_access_signature_key_vault_secret_name = optional(string)
+    tags                                          = optional(map(string))
   }))
 }
 
